0c017852152e648ef6914e03dab9a20709788f21
fig <fig@users.noreply.github.com>
2020-12-06 07:17:16 +0000

    Shebang!

--- 2020/day2/solution.rb
+++ 2020/day2/solution.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 def test_data
   # <<~DATA.freeze
   #   1-3 a: abcde
--- 2020/day5/solution.rb
+++ 2020/day5/solution.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 def test_data
   # <<~DATA
   #   BFFFBBFRRR
