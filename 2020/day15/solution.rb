ac417555f9c887c741090c0605829d4eaea33eef
fig <fig@users.noreply.github.com>
2020-12-15 10:16:14 +0000

    Day 15 part 2 11.3 secs

--- 2020/day15/solution.rb
+++ 2020/day15/solution.rb
@@ -11,33 +11,32 @@ end
 
 DATA = test_data || File.read(File.join(__dir__, "input.txt"))
 
-def next_number(data, tgt:)
-  until data.length == tgt
-    last_spoken = data[-1]
-    data << if data[0..-2].include? last_spoken
-              difference_between_the_turn_number_when_it_was_last_spoken_and_the_turn_number_of_the_time_it_was_most_recently_spoken_before_then(data, last_spoken)
-            else
-              0
-            end
+def play(data, tgt:)
+  data[0..-2].each_with_index do |d, i|
+    @lookup[d] = [i]
   end
-  data.last
-end
-
-def difference_between_the_turn_number_when_it_was_last_spoken_and_the_turn_number_of_the_time_it_was_most_recently_spoken_before_then(data, last_spoken)
-  tmp = data.dup
-  last_spoken_index = tmp.rindex(last_spoken)
-  tmp[last_spoken_index] = "X"
-  previously_spoken_index = tmp.rindex(last_spoken)
-  last_spoken_index - previously_spoken_index
+  current_index = data.size
+  last_spoken = data.last
+  until current_index == tgt
+    next_number = @lookup[last_spoken].empty? ? 0 : (current_index - @lookup[last_spoken].last) - 1
+    @lookup[last_spoken] << current_index - 1
+    current_index += 1
+    last_spoken = next_number
+  end
+  last_spoken
 end
 
 def part1
-  next_number(DATA.strip.split(",").map(&:to_i), tgt: 2_020)
+  @lookup = Hash.new { |hash, key| hash[key] = [] }
+  play(DATA.strip.split(",").map(&:to_i), tgt: 2_020)
 end
 
 def part2
-  # next_number(DATA.strip.split(",").map(&:to_i), tgt: 30_000_000)
+  @lookup = Hash.new { |hash, key| hash[key] = [] }
+  play(DATA.strip.split(",").map(&:to_i), tgt: 30_000_000)
 end
 
-puts "Solution part1:\n#{part1}"
+# puts "Solution part1:\n#{part1}"
+start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 puts "Solution part2:\n#{part2}"
+puts "#{(Process.clock_gettime(Process::CLOCK_MONOTONIC) - start).round 1} secs"
