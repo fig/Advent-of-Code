09f1478ad2222b519655083e5541f2e87e3c9e82
fig <fig@users.noreply.github.com>
2020-12-15 15:50:14 +0000

    Day 15 part 2 6.0 secs

--- 2020/day15/solution.rb
+++ 2020/day15/solution.rb
@@ -13,13 +13,13 @@ DATA = test_data || File.read(File.join(__dir__, "input.txt"))
 
 def play(data, tgt:)
   data[0..-2].each.with_index(1) do |d, i|
-    @lookup[d] = [i]
+    @lookup[d] = i
   end
   current_index = data.size
   last_spoken = data.last
   until current_index == tgt
-    next_number = @lookup[last_spoken].empty? ? 0 : (current_index - @lookup[last_spoken][-1])
-    @lookup[last_spoken] << current_index
+    next_number = @lookup[last_spoken] ? (current_index - @lookup[last_spoken]) : 0
+    @lookup[last_spoken] = current_index
     current_index += 1
     last_spoken = next_number
   end
@@ -27,16 +27,17 @@ def play(data, tgt:)
 end
 
 def part1
-  @lookup = Hash.new { |hash, key| hash[key] = [] }
+  @lookup = {}
+  # @lookup = Hash.new { |hash, key| hash[key] = [] }
   play(DATA.strip.split(",").map(&:to_i), tgt: 2_020)
 end
 
 def part2
-  @lookup = Hash.new { |hash, key| hash[key] = [] }
+  @lookup = {}
   play(DATA.strip.split(",").map(&:to_i), tgt: 30_000_000)
 end
 
-# puts "Solution part1:\n#{part1}"
 start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+# puts "Solution part1:\n#{part1}"
 puts "Solution part2:\n#{part2}"
 puts "#{(Process.clock_gettime(Process::CLOCK_MONOTONIC) - start).round 1} secs"
