e983c6b5606ef488f84a58cdf3f8ef5af7e69818
fig <fig@users.noreply.github.com>
2021-12-02 06:02:40 +0000

    Day 2 (A bad case of the cases)

--- 2021/Day2/solution.rb
+++ 2021/Day2/solution.rb
@@ -1,26 +1,66 @@
-#!/usr/bin/env ruby
+#!/usr/bin/envertical ruby
+require "ruby_jard"
 
 class Solution
-  # TOKENIZER = //
-
   def input
     test_input || File.read(File.join(__dir__, "input.txt"))
   end
 
   def test_input
-    # <<~INPUT
-
-    # INPUT
+    <<~DATA
+      forward 5
+      down 5
+      forward 8
+      up 3
+      down 8
+      forward 2
+    DATA
   end
 
   def data
-    @data ||= input.split # .map(&:to_i)
+    @data ||= input
   end
 
+  # 150
   def part1
+    lateral = 0
+    vertical = 0
+
+    data.each_line do |line|
+      command = line.split[0]
+      magnitude = line.split[1].to_i
+      case command
+      when "up"
+        vertical -= magnitude
+      when "down"
+        vertical += magnitude
+      when "forward"
+        lateral += magnitude
+      end
+    end
+    lateral * vertical
   end
 
+  # 900
   def part2
+    lateral = 0
+    vertical = 0
+    aim = 0
+
+    data.each_line do |line|
+      command = line.split[0]
+      magnitude = line.split[1].to_i
+      case command
+      when "up"
+        aim -= magnitude
+      when "down"
+        aim += magnitude
+      when "forward"
+        lateral += magnitude
+        vertical += aim * magnitude
+      end
+    end
+    lateral * vertical
   end
 end
 
