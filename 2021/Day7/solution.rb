ea418512599b77e3b437275a89506911e7c4d398
fig <fig@users.noreply.github.com>
2021-12-08 01:17:25 +0000

    Day 7 optimisations

--- 2021/Day7/solution.rb
+++ 2021/Day7/solution.rb
@@ -20,9 +20,29 @@ class Solution
                         .min
   end
 
+  def part1_optimised
+    data.sum { |e| (e - median(data)).abs }
+  end
+
   def part2
     (data.min..data.max).map { |position| data.sum { |e| triangle((e - position).abs) } }
                         .min
+  end
+
+  def part2_optimised
+    [mean(data).floor, mean(data).ceil]
+      .map { |position| data.sum { |e| triangle((e - position).abs) } }
+      .min
+  end
+
+  def median(array)
+    sorted = array.sort
+    len = sorted.length
+    (sorted[(len - 1) / 2] + sorted[len / 2]) / 2
+  end
+
+  def mean(array)
+    array.sum / array.length.to_f
   end
 
   def triangle(n)
@@ -31,4 +51,7 @@ class Solution
 end
 
 puts "Part1: #{Solution.new.part1}"
+puts "Part1: #{Solution.new.part1_optimised}"
+puts
 puts "Part2: #{Solution.new.part2}"
+puts "Part2: #{Solution.new.part2_optimised}"
