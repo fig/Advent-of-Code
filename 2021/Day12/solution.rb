9abf11584cf35309d3b3d910658b4df23d799775
fig <fig@users.noreply.github.com>
2021-12-13 22:30:24 +0000

    Day 12

--- 2021/Day12/solution.rb
+++ 2021/Day12/solution.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-require "ruby_jard"
 
 class Solution
   def initialize
@@ -12,29 +11,32 @@ class Solution
     end
   end
 
-  #   start => [A b],
-  #   A     => [start c b end],
-  #   b     => [start A d end],
-  #   c     => [A],
-  #   d     => [b],
-  #   end   => [A b],
-
   def part1
     path = ["start"]
-    count_paths(path)
+    count_paths(path) do |dest, current_path|
+      dest == dest.downcase && current_path.include?(dest)
+    end
   end
 
   def part2
+    path = ["start"]
+    count_paths(path) do |dest, current_path|
+      dest == "start" || (
+        dest == dest.downcase &&
+        current_path.tally.any? { |k, v| k == k.downcase && v > 1 } &&
+        current_path.include?(dest)
+      )
+    end
   end
 
   private
 
-  def count_paths(path)
+  def count_paths(path, &block)
     return 1 if path.last == "end"
 
     @paths[path.last]
-      .reject { |dest| dest == dest.downcase && path.include?(dest) }
-      .sum { |dest| count_paths(path + [dest]) }
+      .reject { |dest| yield dest, path }
+      .sum { |dest| count_paths(path + [dest], &block) }
   end
 
   def input
@@ -42,28 +44,16 @@ class Solution
   end
 
   def test_input
-    <<~INPUT
-      start-A
-      start-b
-      A-c
-      A-b
-      b-d
-      A-end
-      b-end
-    INPUT
-    nil
+    # <<~INPUT
+    #   start-A
+    #   start-b
+    #   A-c
+    #   A-b
+    #   b-d
+    #   A-end
+    #   b-end
+    # INPUT
   end
 end
 puts "Part1: #{Solution.new.part1}"
 puts "Part2: #{Solution.new.part2}"
-
-# start,A,b,A,c,A,end
-# start,A,b,A,end
-# start,A,b,end
-# start,A,c,A,b,A,end
-# start,A,c,A,b,end
-# start,A,c,A,end
-# start,A,end
-# start,b,A,c,A,end
-# start,b,A,end
-# start,b,end
