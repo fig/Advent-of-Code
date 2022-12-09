a1f2355cc86b25e2bb7cd544062ae3e5e434851e
fig <fig@users.noreply.github.com>
2022-12-09 12:57:05 +0000

    Add elegant solution by /u/tobyaw

--- 2022/Day8/solution.rb
+++ 2022/Day8/solution.rb
@@ -111,3 +111,23 @@ puts "Part1: #{Solution.new.part1}"
 puts "Part2: #{Solution.new.part2}"
 # 1827
 # 335580
+
+## Super elegant solution by /u/tobyaw
+## [https://www.reddit.com/r/adventofcode/comments/zfpnka/2022_day_8_solutions/ize38wc/]
+## [https://github.com/tobyaw/advent-of-code-2022]
+#
+# def process(row)
+#   row.reduce([-1]) do |memo, cell|
+#     cell[:visible] = true if cell[:height] > memo.max
+#     cell[:scores] << ((memo.index { |i| i >= cell[:height] } || (memo.size - 2)) + 1)
+#     [cell[:height]] + memo
+#   end
+# end
+
+# input = File.readlines('day_08_input.txt', chomp: true)
+#             .map { |i| i.chars.map { |j| { height: j.to_i, visible: false, scores: [] } } }
+
+# [input, input.transpose].each { |i| i.each { |j| [j, j.reverse].each { |k| process k } } }
+
+# puts input.flatten.select { |i| i[:visible].eql? true }.count
+# puts input.flatten.map { |i| i[:scores].reduce(:*) }.max
