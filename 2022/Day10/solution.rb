ad4fe63327f8d2a9e3ad28122a74fe0e4b7cb3b8
fig <fig@users.noreply.github.com>
2022-12-15 13:58:29 +0000

    Day 10

--- 2022/Day10/solution.rb
+++ 2022/Day10/solution.rb
@@ -1,6 +1,49 @@
 class Solution
+  def solutions
+    puts "Part1: #{part1}"
+    puts "Part2: \n#{part2}"
+  end
+
+  private
+
+  def part1
+    run_program
+    [20, 60, 100, 140, 180, 220].sum { |i| i * register[i] }
+  end
+
+  def part2
+    draw_screen
+    screen.each_slice(40).map(&:join).join("\n")
+  end
+
+  def run_program
+    input.each_line(chomp: true) do |line|
+      register << register.last
+      case line.split
+      in ["addx", x]
+        register << (register.last + x.to_i)
+      else
+        # noop
+      end
+    end
+  end
+
+  def draw_screen
+    index = 1
+    6.times do
+      0.upto(39) do |pixel|
+        mid_sprite = register[index]
+        screen << if [mid_sprite.pred, mid_sprite, mid_sprite.succ].any?(pixel)
+                    "⬜"
+                  else
+                    "⬛"
+                  end
+        index += 1
+      end
+    end
+  end
+
   def input
-    # data ||
     File.read(File.join(__dir__, "input.txt"))
   end
 
@@ -8,186 +51,12 @@ class Solution
     @register ||= [0, 1]
   end
 
-  def run_program
-    input.each_line(chomp: true) do |line|
-      r = register.last
-      register << r
-      case line.split
-      in ["addx", x]
-        register << (r + x.to_i)
-      else
-        # noop
-      end
-    end
-    register
-  end
-
-  def solutions
-    puts "Part1: #{part1}"
-    puts "Part2: #{part2}"
-  end
-
-  private
-
-  def part1
-    run_program
-    # 21, 19, 18, 21, 16, 18
-    [20, 60, 100, 140, 180, 220].sum { |i| i * register[i] }
-  end
-
-  def part2
-  end
-
-  def data
-    <<~DATA
-      addx 15
-      addx -11
-      addx 6
-      addx -3
-      addx 5
-      addx -1
-      addx -8
-      addx 13
-      addx 4
-      noop
-      addx -1
-      addx 5
-      addx -1
-      addx 5
-      addx -1
-      addx 5
-      addx -1
-      addx 5
-      addx -1
-      addx -35
-      addx 1
-      addx 24
-      addx -19
-      addx 1
-      addx 16
-      addx -11
-      noop
-      noop
-      addx 21
-      addx -15
-      noop
-      noop
-      addx -3
-      addx 9
-      addx 1
-      addx -3
-      addx 8
-      addx 1
-      addx 5
-      noop
-      noop
-      noop
-      noop
-      noop
-      addx -36
-      noop
-      addx 1
-      addx 7
-      noop
-      noop
-      noop
-      addx 2
-      addx 6
-      noop
-      noop
-      noop
-      noop
-      noop
-      addx 1
-      noop
-      noop
-      addx 7
-      addx 1
-      noop
-      addx -13
-      addx 13
-      addx 7
-      noop
-      addx 1
-      addx -33
-      noop
-      noop
-      noop
-      addx 2
-      noop
-      noop
-      noop
-      addx 8
-      noop
-      addx -1
-      addx 2
-      addx 1
-      noop
-      addx 17
-      addx -9
-      addx 1
-      addx 1
-      addx -3
-      addx 11
-      noop
-      noop
-      addx 1
-      noop
-      addx 1
-      noop
-      noop
-      addx -13
-      addx -19
-      addx 1
-      addx 3
-      addx 26
-      addx -30
-      addx 12
-      addx -1
-      addx 3
-      addx 1
-      noop
-      noop
-      noop
-      addx -9
-      addx 18
-      addx 1
-      addx 2
-      noop
-      noop
-      addx 9
-      noop
-      noop
-      noop
-      addx -1
-      addx 2
-      addx -37
-      addx 1
-      addx 3
-      noop
-      addx 15
-      addx -21
-      addx 22
-      addx -6
-      addx 1
-      noop
-      addx 2
-      addx 1
-      noop
-      addx -10
-      noop
-      noop
-      addx 20
-      addx 1
-      addx 2
-      addx 2
-      addx -6
-      addx -11
-      noop
-      noop
-      noop
-    DATA
+  def screen
+    @screen ||= []
   end
 end
 
 Solution.new.solutions
+
+# 14620
+# BJFRHRFU
