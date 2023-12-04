76c8e12134bda9a678c30d63416950f5c1149085
fig <fig@users.noreply.github.com>
2023-12-04 21:50:18 +0000

    Day 04 part2

--- 2023/Day04/solution.rb
+++ 2023/Day04/solution.rb
@@ -10,17 +10,27 @@ class Solution
   end
 
   def sample
-    <<~SAMPLE
-      Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-      Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-      Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-      Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-      Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-      Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-    SAMPLE
+    # <<~SAMPLE
+    #   Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    #   Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    #   Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    #   Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    #   Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    #   Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    # SAMPLE
   end
 
   def part2
+    multipliers = Hash.new(1)
+    total = 0
+    cards.each do |card|
+      card_id = card[/Card\s+(\d+)/, 1].to_i
+      total += multipliers[card_id]
+      mine, winning = card.split(":").last.split("|").map(&:split)
+      matching = (mine & winning).size
+      1.upto(matching) { |i| multipliers[card_id + i] += multipliers[card_id] }
+    end
+    total
   end
 
   def part1
@@ -35,7 +45,7 @@ class Solution
 
   def solutions
     puts "Part1: #{part1}"
-    # puts "Part2: #{part2}"
+    puts "Part2: #{part2}"
   end
 end
 
