66fe24b0db3fd6753291b77c9f4c55e9c82d82d8
fig <fig@users.noreply.github.com>
2024-12-02 02:57:59 +0000

    2024 Day 01

---
+++ 2024/Day01/solution.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+class Solution
+  def input
+    File.read(File.join(__dir__, "input.txt")).split("\n")
+  end
+
+  def solutions
+    puts "Part1: #{part1} #{part1 == 2_815_556 ? '✅' : '❌'}"
+    puts "Part2: #{part2} #{part2 == 23_927_637 ? '✅' : '❌'}"
+  end
+
+private
+
+  def part1
+    input.map { |line| line.split.map(&:to_i) }
+         .transpose.map!(&:sort!)
+         .transpose.sum { |row| (row.first - row.last).abs }
+  end
+
+  def part2
+    input.map { |line| line.split.map(&:to_i) }
+         .transpose
+         .then { |left, right| left.sum { |num| num * right.count(num) } }
+  end
+end
+
+Solution.new.solutions
