9c69f3a09e941eaefa17421c87ccb460de595fac
fig <fig@users.noreply.github.com>
2024-12-11 21:09:15 +0000

    2024 Day 11

--- 2024/Day11/solution.rb
+++ 2024/Day11/solution.rb
@@ -1,45 +1,49 @@
 #!/usr/bin/env ruby
 
 class Solution
+  attr_reader :count
+
   def input
-    "125 17"
-    # File.read(File.join(__dir__, "input.txt"))
+    # "125 17"
+    # "512 72 2024 2 0 2 4 2867 6032"
+    File.read(File.join(__dir__, "input.txt"))
   end
 
-  class Rock
-    attr_accessor :value
+  def tick(value, level, cost_value = 1)
+    return cost_value if level.zero?
 
-    def initialize(value)
-      @value = value.to_i
-    end
-
-    def tick
-      if @value.zero?
-        Rock.new(1)
-      elsif @value.to_s.length.even?
-        s = @value.to_s
+    cache[[level, value]] ||=
+      if value.zero?
+        tick(1, level - 1, cost_value)
+      elsif value.to_s.length.even?
+        s = value.to_s
         l = s.length / 2
-        [Rock.new(s[...l]), Rock.new(s[l..])]
+        tick(s[...l].to_i, level - 1, cost_value) + tick(s[l..].to_i, level - 1, cost_value)
       else
-        Rock.new(@value * 2024)
+        tick(value * 2024, level - 1, cost_value)
       end
-    end
   end
 
   def blink(n, line)
-    n.times do |i|
-      line.map!(&:tick)
-      line.flatten!
-    end
-    line.size
+    line.sum { |rock| tick(rock, n) }
   end
 
+  def cache
+    @cache ||= {}
+    # @cache ||= Hash.new { |h, k| h[k] = {} }
+  end
+
+  # def cost_values
+  #   @cost_values ||= Hash.new { |h, k| h[k] = Hash.new(0) }
+  # end
+
   def part1
-    line = input.chomp.split.map! { |v| Rock.new(v) }
+    line = input.chomp.split.map(&:to_i)
     blink(25, line)
   end
 
   def part2
+    line = input.chomp.split.map(&:to_i)
     blink(75, line)
   end
 
@@ -49,8 +53,8 @@ class Solution
     p1 = part1
     puts "Part1: #{p1} #{[55_312, 189_092].include?(p1) ? '✅' : '❌'}"
 
-    # p2 = part2
-    # puts "Part2: #{p2} #{[65_601_038_650_482].include?(p2) ? '✅' : '❌'}"
+    p2 = part2
+    puts "Part2: #{p2} #{[65_601_038_650_482].include?(p2) ? '✅' : '❌'}"
 
     finish = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     puts "\nExecution time: #{((finish - start) * 1000).round(2)} ms"
