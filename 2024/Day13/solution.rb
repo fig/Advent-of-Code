60016ef7911f2e9452e07d7e9450dfee0ab8c46f
fig <fig@users.noreply.github.com>
2024-12-13 16:55:22 +0000

    2024 Day 13

--- 2024/Day13/solution.rb
+++ 2024/Day13/solution.rb
@@ -20,67 +20,56 @@ class Solution
       Button B: X+27, Y+71
       Prize: X=18641, Y=10279
     INPUT
-    # File.read(File.join(__dir__, "input.txt"))
+    File.read(File.join(__dir__, "input.txt"))
   end
 
   def machines
-    @machines ||= input.split("\n\n")
+    @machines ||=
+      input.split("\n\n").map { |machine|
+        machine.split("\n").map { |line| line.scan(/\d+/).map(&:to_i) }
+               .then do |a, b, prize|
+                 {
+                   a:     { x: a[0], y: a[1] },
+                   b:     { x: b[0], y: b[1] },
+                   prize: { x: prize[0], y: prize[1] },
+                 }
+               end
+      }
   end
 
-  def cramer
-    #     A = (p_x*b_y - prize_y*b_x) / (a_x*b_y - a_y*b_x)
-    #     B = (a_x*p_y - a_y*p_x) / (a_x*b_y - a_y*b_x)
-    lambda do |ax, ay, bx, by, px, py|
-      [
-        ((px * by) - (py * bx)) / ((ax * by) - (ay * bx)),
-        ((ax * py) - (ay * px)) / ((ax * by) - (ay * bx)),
-      ]
-    end
-  end
+  def cramers_rule
+    lambda  { |machine, offset: 0|
+      prize = { x: machine[:prize][:x] + offset, y: machine[:prize][:y] + offset }
+      det = (machine[:a][:x] * machine[:b][:y]) - (machine[:a][:y] * machine[:b][:x])
+      return 0 if det.zero?
 
-  #   fn solve_machine(machine: &Machine, offset: isize) -> isize {
-  #     let prize = (machine.prize.0, machine.prize.1);
-  #     let det = machine.a.0 * machine.b.1 - machine.a.1 * machine.b.0;
-  #     let a = (prize.0 * machine.b.1 - prize.1 * machine.b.0) / det;
-  #     let b = (machine.a.0 * prize.1 - machine.a.1 * prize.0) / det;
-  #     if (machine.a.0 * a + machine.b.0 * b, machine.a.1 * a + machine.b.1 * b) == (prize.0, prize.1) {
-  #         a * 3 + b
-  #     } else {
-  #         0
-  #     }
-  # }
-  def solve_machine
-    lambda do |a, b, prize|
-      det = (a[0] * b[1]) - (a[1] * b[0])
-      a = ((prize[0] * b[1]) - (prize[1] * b[0])) / det
-      b = ((a[0] * prize[1]) - (a[1] * prize[0])) / det
-      if [((a[0] * a) + (b[0] * b)), ((a[1] * a) + (b[1] * b))] == prize
+      a = ((prize[:x] * machine[:b][:y]) - (prize[:y] * machine[:b][:x])) / det
+      b = ((machine[:a][:x] * prize[:y]) - (machine[:a][:y] * prize[:x])) / det
+      if [
+        (machine[:a][:x] * a) + (machine[:b][:x] * b),
+        (machine[:a][:y] * a) + (machine[:b][:y] * b),
+      ] == prize.values
+
         (a * 3) + b
       else
         0
       end
-    end
+    }
   end
 
   def part1
-    machines.map! do |machine|
-      a, b, prize =
-        machine.split("\n").map { |line|
-        x, y = line.scan(/\d+/).map(&:to_i)
-        [x, y]
-      }
-
-      solve_machine.call(a, b, prize)
-    end
+    machines.sum(&cramers_rule)
   end
 
   def part2
+    machines.sum { |machine| cramers_rule.call(machine, offset: 10_000_000_000_000) }
   end
 
   def solutions
     p1 = part1
-    puts "Part1: #{p1} #{[480].include?(p1) ? '✅' : '❌'}"
-    puts "Part2: #{part2}"
+    p2 = part2
+    puts "Part1: #{p1} #{[480, 28_753].include?(p1) ? '✅' : '❌'}"
+    puts "Part2: #{p2} #{[875_318_608_908, 102_718_967_795_500].include?(p2) ? '✅' : '❌'}"
   end
 end
 
